ad7b85cd9e5ace948288709b9efe0d1637b0c748
sunilgaru1 <sunilgaru097@gmail.com>
2026-05-30 14:57:10 +0530

    Added LC 50 Pow(X,n)

---
+++ Recursion/LC_50 _Pow_X_n.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    double power(double x,long long n){
+        if(n==0){
+            return 1;
+        }
+        double half = power(x,n/2);
+        if(n%2==0){
+            return half*half;
+        }
+        else{
+            return x * half * half;
+        }
+    }
+    double myPow(double x, int n) {
+        long long N = n;
+        if(N==0){
+            return 1;
+        }
+        else if(N==1){
+            return x;
+        }
+        if(N<0){
+            N = -N;
+            x = 1/x;
+        }
+        return power(x,N);
+    }
+};
