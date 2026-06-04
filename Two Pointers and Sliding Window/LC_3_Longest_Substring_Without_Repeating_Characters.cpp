22cc97148dae446c1ebdfa362917f16c36db8393
sunilgaru1 <sunilgaru097@gmail.com>
2026-06-04 16:09:38 +0530

    Added LC 1614. Maximum Nesting Depth of the Parentheses

--- Two Pointers and Sliding Window/LC_3_Longest_Substring_Without_Repeating_Characters.cpp
+++ Two Pointers and Sliding Window/LC_3_Longest_Substring_Without_Repeating_Characters.cpp
@@ -1,1 +1,26 @@
-3. Longest Substring Without Repeating Characters
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int n = s.size();
+        if(n==0 || n==1){
+            return n;
+        }
+        int l = 0;
+        int r = 1;
+        int maxi = 1;
+        while(r!=n){
+            int i = l;
+            while(i<r){
+                if(s[r]==s[i]){
+                    l = i+1;
+                    break;
+                }
+                i++;
+            }
+            maxi = max(r+1-l,maxi);
+            r++;
+        }
+        return maxi;
+    }
+};
