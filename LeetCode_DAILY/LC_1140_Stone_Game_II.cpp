a0fdd299d5b0dc38d9a6a0010dbf128ba6a1f93c
sunilgaru1 <sunilgaru097@gmail.com>
2026-08-12 23:42:26 +0530

    Added LC 2958. Length of Longest Subarray With at Most K Frequency

--- LeetCode_DAILY/LC_1140_Stone_Game_II.cpp
+++ LeetCode_DAILY/LC_1140_Stone_Game_II.cpp
@@ -1,1 +1,23 @@
-v
+class Solution {
+public:
+    int t[2][101][101];
+    int score(int person ,int i,int M,vector<int> &piles){
+        if(i>=piles.size()) return 0;
+        if(t[person][i][M] != (-1)) return t[person][i][M];
+        int taken = (person==1)?(-1):INT_MAX;
+        int stones = 0;
+        for(int X = 1; X <= min(2*M,(int)piles.size()-i) ; X++){
+            stones += piles[i+X-1];
+            if(person==1){
+                taken = max(taken ,stones + score(0,i+X,max(M,X),piles));
+            }else {
+                taken = min(taken,score(1,i+X,max(M,X),piles));
+            }
+        }
+        return t[person][i][M] = taken;
+    }
+    int stoneGameII(vector<int>& piles) {
+        memset(t,-1,sizeof(t));
+        return score(1,0,1,piles);
+    }
+};
